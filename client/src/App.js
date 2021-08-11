2933cfc17b9f9fe272137c7047625681d4161671
fadilaharifki <fadilah.arifki29@gmail.com>
2021-08-11 20:07:53 +0700

    first

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles/output.css'
+
+function App() {
+  return (
+    <div className="bg-[#be3a3a]">Home</div>
+  );
+}
+
+export default App;
